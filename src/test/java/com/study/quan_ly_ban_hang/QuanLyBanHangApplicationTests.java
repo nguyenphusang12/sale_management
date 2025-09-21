b55d08d03272728b90c5935b1ef35024278e6120
Sang Nguyen <sang.nguyen2@sotatek.com>
2025-09-21 18:03:29 +0700

    add

---
+++ src/test/java/com/study/quan_ly_ban_hang/QuanLyBanHangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.quan_ly_ban_hang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyBanHangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
