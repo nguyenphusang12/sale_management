7a2c6677bfd41c5be96928146ac4017b1f89408e
Sang Nguyen <sang.nguyen2@sotatek.com>
2025-10-07 08:03:07 +0700

    add

--- src/main/java/com/study/quan_ly_ban_hang/dto/request/ApiResponse.java
+++ src/main/java/com/study/quan_ly_ban_hang/dto/request/ApiResponse.java
@@ -1,34 +1,17 @@
 package com.study.quan_ly_ban_hang.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ApiResponse {
     private int code = 1000;
     private String message;
     private Object data;
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
 }
--- src/main/java/com/study/quan_ly_ban_hang/dto/request/UserCreationRequest.java
+++ src/main/java/com/study/quan_ly_ban_hang/dto/request/UserCreationRequest.java
@@ -1,20 +1,23 @@
 package com.study.quan_ly_ban_hang.dto.request;
 
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserCreationRequest {
     @Size(min = 3, message = "INVALID_USERNAME")
-    private String username;
+    String username;
 
     @Size(min = 8, message = "INVALID_PASSWORD")
-    private String password;
-    private String firstName;
-    private String lastName;
-    private LocalDate dob;
+    String password;
+    String firstName;
+    String lastName;
+    LocalDate dob;
 }
